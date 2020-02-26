4ebe37665eb668ccf78e6c14bcd7d76d81788974
Almarie-Crista Mercado <acmercado3@gmail.com>
2020-02-26 14:37:05 -0800

    updated sketch.js according to video tutorials

--- sketch.js
+++ sketch.js
@@ -1,7 +1,86 @@
+// cats i have loved before
+let cats = [{
+  name: "momo",
+  color: "grey tabby"
+}, {
+  name: "tora",
+  color: "porcupine tabby"
+}, {
+  name: "snowball",
+  color: "white calico"
+}, {
+  name: "wednesday",
+  color: "rainbow calico"
+}, {
+  name: "trowa",
+  color: "grey bombay"
+}, {
+  name: "luna",
+  color: "black bombay"
+}];
+
+let randomIndex;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(200);
+  textSize(32);
+
+  text("click to randomize", 50, 50);
+
+
 }
 
+
 function draw() {
-  background(220);
+
 }
+
+function changeBackground(){
+  if (counter <= 5){
+    counter++;
+    console.log(counter);
+    background(random(255), random(255), random(255));
+    setTimeout(changeBackground, 1000);
+  } else {
+
+  }
+}
+
+function mousePressed() {
+
+if (cats[0]){
+  // displays a random name and splices it out of the array
+  background(random(200, 255));
+  randomIndex = int(random(cats.length));
+  text(`${cats[randomIndex].name} is a
+    ${cats[randomIndex].color}`, 50, 50);
+  // same as above
+      // text(cats[randomIndex].name + "'s color and breed is " + cats[randomIndex].color, 50, 50);
+
+  cats.splice(randomIndex, 1);
+} else {
+  background(random(200, 255));
+  text("nothing left!", 50, 50);
+}
+}
+
+
+// NOTES:
+    // pop kicks out the item that was last
+    // push adds an item in place of what was popped out
+    // shift kicks out the item that was first
+    // unshift adds an item in place of what was shifted out
+    // splice (index, number)
+        // (2, 2) gets rid of the second, then gets rid of another (hence 2)
+        // (4, 1) gets rid of the fourth, and only the fourth
+    // how you access object properties:
+        // console.log(cats[2].color);
+    // calling a random integer:
+        // console.log(cats[int(random(5))]);
+        // random will never return the top most number, will round (decimals)
+        // cats is the array
+    // .length = length of the array
+
+    // setInterval = every second
+    // setTimeout = once (if in function, acts like interval)
